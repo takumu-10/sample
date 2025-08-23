b77dd1535e88bac42c818c92918ec8d203a43db1
DESKTOP-EKQNL3L\ks_05 <s.komoda05@gmail.com>
2025-08-23 19:43:45 +0900

    しゆう/「長袖をください」を追加

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -3,7 +3,7 @@
   <Authenticator>
     <template #default="{ user, signOut }">
       <main style="padding:24px">
-        <h1>Hi, {{ user?.username }}</h1>
+        <h1>Hi, 長袖をください{{ user?.username }}</h1>
         <button @click="signOut">Sign out</button>
       </main>
     </template>
